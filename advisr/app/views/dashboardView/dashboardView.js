8da896e70794904161c8945103d862e429f1fff2
radhajain <jainr@stanford.edu>
2017-11-18 21:01:51 -0800

    Grid layout for dashboard

--- advisr/app/views/dashboardView/dashboardView.js
+++ advisr/app/views/dashboardView/dashboardView.js
@@ -1,14 +1,21 @@
 var view = require("ui/core/view");
 var app = require("tns-core-modules/application");
 var orientationModule = require("nativescript-screen-orientation");
+var observable = require("data/observable");
+var pageData = new observable.Observable();
+
+var page;
 
 exports.pageNavigating = function(args) {
     page = args.object;
-     orientationModule.orientationCleanup();
+    orientationModule.orientationCleanup();
+    page.bindingContext = pageData;
+    var title = "Radha Jain's Progress Report"
+	pageData.set("title", title);
 }
 
 exports.pageLoaded = function(args) {
-	orientationModule.setCurrentOrientation("landscape",function(){
-	    console.log("landscape orientation set");
-	});
+	orientationModule.setCurrentOrientation("landscape");
+	
+	 
 }
