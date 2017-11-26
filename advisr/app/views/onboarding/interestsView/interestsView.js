18dbbc08a27548dbc790ff08d0fe7139c3a798a0
radhajain <jainr@stanford.edu>
2017-11-26 08:28:07 -0800

    List pickers done

--- advisr/app/views/onboarding/interestsView/interestsView.js
+++ advisr/app/views/onboarding/interestsView/interestsView.js
@@ -13,7 +13,9 @@ var container;
 var interests = ["Building with my hands", "Mathematical Proofs", "Current Affairs", "Coding Projects", 
 				"The economy", "Evaluating human perspectives", "Discussing Art" ,"Psychology", "Critiquing Society",
 				"Law & Justice", "Literature", "Complex Electrical Systems", "Filmmaking", "Creating Art",
-				"Clean energy", "Theatre", "Foreign Languages"];
+				"Clean energy", "Theatre", "Foreign Languages", "Human Computer Interaction"];
+var majors = [ "Art", "Anthropology", "Comparative Literature", "Computer Science", "Economics", "History",  "Modern Languages",
+				"MS&E", "Polical Science",  "Psychology"];
 
 exports.pageNavigating = function(args) {
     page = args.object;
@@ -30,6 +32,7 @@ exports.pageLoaded = function(args) {
 	for (var i = 0; i < interests.length; i++) {
 		selectedInterests.push(false);
 	}
+	initPickers();
 	setGrid();
 };
 
@@ -90,6 +93,100 @@ exports.buttonTap = function(args) {
   	 listView.refresh();
 
 }
+
+
+//LIST PICKERS FOR MAJORS/MINORS
+
+
+exports.showMajor1Picker = function() {
+    pageData.set("showMajor1Picker", true);
+}
+
+exports.getMajor1SelectedIndex = function(args) {
+	var picker = page.getElementById("major1picker");
+    pageData.set("major1", majors[picker.selectedIndex]);
+    pageData.set("showMajor1Picker", false);
+}
+
+exports.showMajor2Picker = function() {
+    pageData.set("showMajor2Picker", true);
+}
+
+exports.getMajor2SelectedIndex = function(args) {
+	var picker = page.getElementById("major2picker");
+    pageData.set("major2", majors[picker.selectedIndex]);
+    pageData.set("showMajor2Picker", false);
+}
+
+exports.showMajor3Picker = function() {
+    pageData.set("showMajor3Picker", true);
+}
+
+exports.getMajor3SelectedIndex = function(args) {
+	var picker = page.getElementById("major3picker");
+    pageData.set("major3", majors[picker.selectedIndex]);
+    pageData.set("showMajor3Picker", false);
+}
+
+exports.showMinor1Picker = function() {
+    pageData.set("showMinor1Picker", true);
+}
+
+exports.getMinor1SelectedIndex = function(args) {
+	var picker = page.getElementById("minor1picker");
+    pageData.set("minor1", majors[picker.selectedIndex]);
+    pageData.set("showMinor1Picker", false);
+}
+
+exports.showMinor2Picker = function() {
+    pageData.set("showMinor2Picker", true);
+}
+
+exports.getMinor2SelectedIndex = function(args) {
+	var picker = page.getElementById("minor2picker");
+    pageData.set("minor2", majors[picker.selectedIndex]);
+    pageData.set("showMinor2Picker", false);
+}
+
+exports.showMinor3Picker = function() {
+    pageData.set("showMinor3Picker", true);
+}
+
+exports.getMinor3SelectedIndex = function(args) {
+	var picker = page.getElementById("minor3picker");
+    pageData.set("minor3", majors[picker.selectedIndex]);
+    pageData.set("showMinor3Picker", false);
+}
+
+
+
+exports.addMajorField = function() {
+	pageData.set("showMajor3", true);
+}
+
+exports.addMinorField = function() {
+	pageData.set("showMinor3", true);
+}
+
+
+var initPickers = function () {
+  	pageData.set("majorList", majors);
+  	pageData.set("showMajor1Picker", false);
+  	pageData.set("showMinor1Picker", false);
+  	pageData.set("showMajor2Picker", false);
+  	pageData.set("showMinor2Picker", false);
+  	pageData.set("showMajor3Picker", false);
+  	pageData.set("showMinor3Picker", false);
+  	pageData.set("showMajor3", false);
+  	pageData.set("showMinor3", false);
+  	pageData.set("major1", "Select a major");
+	pageData.set("major2", "Select a major");
+	pageData.set("major3", "Select a major");
+	pageData.set("minor1", "Select a minor");
+	pageData.set("minor2", "Select a minor");
+	pageData.set("minor3", "Select a minor");
+}
+
 
 exports.finish = function() {
 	frameModule.topmost().navigate("views/dashboardView/dashboardView");
