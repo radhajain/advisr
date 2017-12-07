2a8511d525b8e204d5e8beddb5245ee05da58ded
Anna Carroll <anna3077@stanford.edu>
2017-12-06 16:05:59 -0800

    styling on boarding, clean up some backend code

--- advisr/app/views/requirementsDetailView/requirementsDetailView.js
+++ advisr/app/views/requirementsDetailView/requirementsDetailView.js
@@ -33,54 +33,65 @@ exports.pageLoaded = function(args) {
 var setGrid = function(classes, id, core) {
   var classToPush = [];
   var temp;
-  if (core) {
-    var remaining = classes[0].core.length%3;
-    for (var i = 0; i < classes[0].core.length; i++) {
-      if (i % 3 == 0) {
-        temp = {one: classes[0].core[i][0]};
-      } else if (i % 3 == 1) {
-        temp.two = classes[0].core[i][0];
-      } else {
-        temp.three = classes[0].core[i][0];
-        classToPush.push(temp);
-      }
-    } 
-    if (remaining == 1) {
-      temp = {one: classes[0].core[classes[0].core.length - 1][0]};
-      temp.two= "";
-      temp.three = "";
-      classToPush.push(temp);
-    } else if (remaining == 2) {
-      temp = {one: classes[0].core[classes[0].core.length - 2][0]};
-      temp.two = classes[0].core[classes[0].core.length - 1][0];
-      temp.three = "";
-      classToPush.push(temp);
-    }
-    pageData.set(id, classToPush);
+
+  var classArr = [];
+  if(core){
+    classArr = classes[0].core;
   } else {
-    var remaining = classes[1].electives.length%3;
-    for (var i = 0; i < classes[1].electives.length; i++) {
+    classArr = classes[1].electives;
+  }
+
+  // if (core) {
+    var remaining = classArr.length%3;
+    for (var i = 0; i < classArr.length; i++) {
       if (i % 3 == 0) {
-        temp = {one: classes[1].electives[i][0]};
+        temp = {one: classArr[i][0]};
       } else if (i % 3 == 1) {
-        temp.two = classes[1].electives[i][0];
+        temp.two = classArr[i][0];
       } else {
-        temp.three = classes[1].electives[i][0];
+        temp.three = classArr[i][0];
         classToPush.push(temp);
       }
     } 
     if (remaining == 1) {
-      temp = {one: classes[1].electives[classes[1].electives.length - 1][0]};
+      temp = {one: classArr[classArr.length - 1][0]};
       temp.two= "";
       temp.three = "";
       classToPush.push(temp);
     } else if (remaining == 2) {
-      temp = {one: classes[1].electives[classes[1].electives.length - 2][0]};
-      temp.two = classes[1].electives[classes[1].electives.length - 1][0];
+      temp = {one: classArr[classArr.length - 2][0]};
+      temp.two = classArr[classArr.length - 1][0];
       temp.three = "";
       classToPush.push(temp);
     }
     pageData.set(id, classToPush);
-  }
+  // } 
+
+
+  // else {
+  //   var remaining = classes[1].electives.length%3;
+  //   for (var i = 0; i < classes[1].electives.length; i++) {
+  //     if (i % 3 == 0) {
+  //       temp = {one: classes[1].electives[i][0]};
+  //     } else if (i % 3 == 1) {
+  //       temp.two = classes[1].electives[i][0];
+  //     } else {
+  //       temp.three = classes[1].electives[i][0];
+  //       classToPush.push(temp);
+  //     }
+  //   } 
+  //   if (remaining == 1) {
+  //     temp = {one: classes[1].electives[classes[1].electives.length - 1][0]};
+  //     temp.two= "";
+  //     temp.three = "";
+  //     classToPush.push(temp);
+  //   } else if (remaining == 2) {
+  //     temp = {one: classes[1].electives[classes[1].electives.length - 2][0]};
+  //     temp.two = classes[1].electives[classes[1].electives.length - 1][0];
+  //     temp.three = "";
+  //     classToPush.push(temp);
+  //   }
+  //   pageData.set(id, classToPush);
+  // }
   
 };
